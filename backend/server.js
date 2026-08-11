de628146b35a80e4ec202672bf19129252ea5332
Harshit Gupta <harshit.merndev@gmail.com>
2026-08-11 13:33:16 +0530

    add telemetry model controller and routes

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import telemetryRoutes from "./routes/telemetry.route.js";
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/telemetry", telemetryRoutes);
 app.get("/", (req, res) => {
   res.send("NexusFlow Backend is running");
 });
