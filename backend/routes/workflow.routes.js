617b0552b68630488d0d98b863d5f93379a6bfe4
Harshit Gupta <harshit.merndev@gmail.com>
2026-08-13 21:28:49 +0530

    add workflow compiler

--- backend/routes/workflow.routes.js
+++ backend/routes/workflow.routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createWorkflow,
   getWorkflows,
+  compileWorkflowById
 } from "../controllers/workflow.controller.js";
 
 const router = express.Router();
 
 router.post("/", createWorkflow);
+router.post("/:id/compile", compileWorkflowById);
 router.get("/", getWorkflows);
 
 export default router;
---
+++ backend/services/workflowCompiler.js
@@ -0,0 +1,28 @@
+export const compileWorkflow = (workflow) => {
+  const { nodes, edges } = workflow;
+
+  const compiledSteps = [];
+
+  let currentNode = nodes.find(
+    (node) => node.type === "start"
+  );
+
+  while (currentNode) {
+    compiledSteps.push({
+      id: currentNode.id,
+      type: currentNode.type,
+    });
+
+    const nextEdge = edges.find(
+      (edge) => edge.source === currentNode.id
+    );
+
+    if (!nextEdge) break;
+
+    currentNode = nodes.find(
+      (node) => node.id === nextEdge.target
+    );
+  }
+
+  return compiledSteps;
+};
